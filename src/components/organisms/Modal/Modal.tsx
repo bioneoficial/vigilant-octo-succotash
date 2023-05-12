2dbc5f4ac5a158178ade50fc6f26dbf11a9a971c
Joao Bione <79943267+bioneoficial@users.noreply.github.com>
2023-05-12 17:08:39 -0300

    Div centralizada no Modal MainTemplate

--- src/components/organisms/Modal/Modal.tsx
+++ src/components/organisms/Modal/Modal.tsx
@@ -15,33 +15,33 @@ export const Modal: React.FC<ModalProps> = ({
 
   return (
     <div
-      className=" z-99 inset-0 "
+      className="fixed flex justify-center items-center z-50 inset-0 overflow-y-auto"
       aria-labelledby="modal-title"
       role="dialog"
       aria-modal="true"
     >
-      <div className="   flex sm:block sm:p-0">
+      <div className=" min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
         <div
-          className=" inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+          className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
           aria-hidden="true"
         ></div>
+      </div>
 
-        <div className="  inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
-          <div>
-            <div className="mt-3 text-center sm:mt-5">
-              <h3
-                className="text-lg leading-6 font-medium text-gray-900"
-                id="modal-title"
-              >
-                {title}
-              </h3>
-              <div className="mt-2">
-                <p className="text-sm text-gray-500">{description}</p>
-              </div>
+      <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+        <div>
+          <div className="mt-3 text-center sm:mt-5">
+            <h3
+              className="text-lg leading-6 font-medium text-gray-900"
+              id="modal-title"
+            >
+              {title}
+            </h3>
+            <div className="mt-2">
+              <p className="text-sm text-gray-500">{description}</p>
             </div>
           </div>
-          {children}
         </div>
+        {children}
       </div>
     </div>
   );
