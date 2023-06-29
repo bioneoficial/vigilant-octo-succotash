e0693172a21e5d0996ee9fe5fe109b3ce9a3fa0e
Joao Bione <79943267+bioneoficial@users.noreply.github.com>
2023-06-29 10:36:14 -0300

    feat: Swiper Slide working

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     domains: ["media.licdn.com", "www.gravatar.com", "i.imgur.com", "d34oo2ynf8ecvf.cloudfront.net"],
   },
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,47 @@
-import "swiper/swiper.css"; // core Swiper
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation, Pagination } from "swiper";
+
+// Import Swiper styles
+import "swiper/swiper-bundle.css";
+import Image from "next/image";
 import { HeaderHome } from "@/components/organisms/HeaderHome/HeaderHome";
 
-export default function Home(): JSX.Element {
-  // Array of slide properties for rendering the slides
-  // const slides = [
-  //   // You can add or remove slides from this array
-  //   {
-  //     imgSrc:
-  //       "https://d34oo2ynf8ecvf.cloudfront.net/production/author-7370/serie-579/banner.jpg",
-  //     alt: "ImgNet",
-  //     link: "https://d34oo2ynf8ecvf.cloudfront.net/production/author-7370/serie-579/banner.jpg",
-  //   },
-  //   {
-  //     imgSrc:
-  //       "https://d34oo2ynf8ecvf.cloudfront.net/production/author-7370/serie-579/banner.jpg",
-  //     alt: "ImgNet",
-  //     link: "https://d34oo2ynf8ecvf.cloudfront.net/production/author-7370/serie-579/banner.jpg",
-  //   },
-  //   {
-  //     imgSrc:
-  //       "https://d34oo2ynf8ecvf.cloudfront.net/production/author-7370/serie-579/banner.jpg",
-  //     alt: "ImgNet",
-  //     link: "https://d34oo2ynf8ecvf.cloudfront.net/production/author-7370/serie-579/banner.jpg",
-  //   },
-  //   // ...
-  // ];
+// install Swiper modules
+SwiperCore.use([Navigation, Pagination]);
 
+export default function Home(): JSX.Element {
   return (
     <div className="flex flex-col">
       <HeaderHome />
+      <div className="relative w-full h-96">
+        <Swiper
+          navigation
+          pagination={{ clickable: true }}
+          className="w-full h-full"
+        >
+          <SwiperSlide className="w-full h-full">
+            <Image
+              src="https://d34oo2ynf8ecvf.cloudfront.net/production/author-7370/serie-579/banner.jpg"
+              alt="ImgNet"
+              fill={true}
+              style={{ objectFit: "fill" }}
+            />
+          </SwiperSlide>
+          <SwiperSlide className="w-full h-full">
+            <Image
+              src="/images/logo-funktoon.svg"
+              alt="ImgNet"
+              fill={true}
+              style={{ objectFit: "fill" }}
+            />
+          </SwiperSlide>
+        </Swiper>
+      </div>
       <div>
         <h4 className="headline-section demi-font mt-3 mb-3 font-bold uppercase">
           Bombando
         </h4>
       </div>
-
-      {/* Repeat the above structure for other sections */}
     </div>
   );
 }
