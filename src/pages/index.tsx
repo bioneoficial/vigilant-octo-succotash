17704c18c315748b9418a7eefcdeb2bd75d23a28
Joao Bione <79943267+bioneoficial@users.noreply.github.com>
2023-06-29 11:59:11 -0300

    responsive Swiper main carrousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,31 +5,85 @@ import SwiperCore, { Navigation, Pagination } from "swiper";
 import "swiper/swiper-bundle.css";
 import Image from "next/image";
 import { HeaderHome } from "@/components/organisms/HeaderHome/HeaderHome";
+import { useEffect, useState } from "react";
 
 // install Swiper modules
 SwiperCore.use([Navigation, Pagination]);
 
 export default function Home(): JSX.Element {
+  const [screenWidth, setScreenWidth] = useState(0);
+
+  useEffect(() => {
+    setScreenWidth(window.innerWidth);
+
+    const handleResize = (): void => {
+      setScreenWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  let swiperHeight = "h-96"; // Default height
+
+  if (screenWidth >= 1300) {
+    swiperHeight = "h-80";
+  } else if (screenWidth >= 1280) {
+    swiperHeight = "h-72";
+  } else if (screenWidth >= 1200) {
+    swiperHeight = "h-64";
+  } else if (screenWidth >= 1100) {
+    swiperHeight = "h-60";
+  } else if (screenWidth >= 1000) {
+    swiperHeight = "h-56";
+  } else {
+    swiperHeight = "h-52";
+  }
+
   return (
     <div className="flex flex-col">
       <HeaderHome />
-      <div className="relative w-full h-96">
+      <div className={`relative w-full ${swiperHeight}`}>
         <Swiper
           navigation
           pagination={{ clickable: true }}
-          className="w-full h-full"
+          slidesPerView={1}
+          breakpoints={{
+            767: {
+              slidesPerView: 3,
+            },
+          }}
+          className="w-full  h-full"
         >
           <SwiperSlide className="w-full h-full">
             <Image
               src="https://d34oo2ynf8ecvf.cloudfront.net/production/author-7370/serie-579/banner.jpg"
               alt="ImgNet"
               fill={true}
+              quality={100}
               style={{ objectFit: "fill" }}
             />
           </SwiperSlide>
           <SwiperSlide className="w-full h-full">
             <Image
               src="/images/logo-funktoon.svg"
+              alt="ImgNet"
+              fill={true}
+              quality={100}
+              style={{ objectFit: "fill" }}
+            />
+          </SwiperSlide>
+          <SwiperSlide className="w-full h-full">
+            <Image
+              src="https://d34oo2ynf8ecvf.cloudfront.net/production/funktoon/serie-300/banner.jpg"
+              alt="ImgNet"
+              fill={true}
+              quality={100}
+              style={{ objectFit: "fill" }}
+            />
+          </SwiperSlide>
+          <SwiperSlide className="w-full h-full">
+            <Image
+              src="https://d34oo2ynf8ecvf.cloudfront.net/production/funktoon/serie-50/banner.jpg"
               alt="ImgNet"
               fill={true}
               style={{ objectFit: "fill" }}
