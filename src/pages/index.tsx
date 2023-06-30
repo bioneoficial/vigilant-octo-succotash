3e62083536ebb9359ad6067bc2f8b03e486d9c90
Joao Bione <79943267+bioneoficial@users.noreply.github.com>
2023-06-30 15:47:01 -0300

    adjust responsive in homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,7 +28,7 @@ export default function Home(): JSX.Element {
   return (
     <div className="flex flex-col">
       <HeaderHome />
-      <div className={` relative w-full h-96`}>
+      <div className=" relative w-full h-96">
         <Swiper
           navigation
           pagination={{ clickable: true, dynamicBullets: true }}
@@ -62,7 +62,7 @@ export default function Home(): JSX.Element {
           ))}
         </Swiper>
       </div>
-      <div className="relative bg-faixa-1 bg-cover  bg-center h-32 lg:max-2xlh-40 md:h-36 sm:h-32 fold:h-24 w-full flex flex-row-reverse justify-around ">
+      <div className="relative bg-faixa-1 bg-cover  bg-center h-32 lg:max-2xl:h-40 md:h-36 sm:h-32 fold:h-24 w-full flex flex-row-reverse justify-around ">
         <div className="relative w-1/6 h-auto">
           <Image
             src="/images/FKTN.svg"
@@ -84,41 +84,293 @@ export default function Home(): JSX.Element {
           />
         </div>
       </div>
-      <div>
-        <h4 className="headline-section demi-font mt-3 mb-3 font-bold uppercase">
-          Bombando
-        </h4>
-      </div>
-      <div>
-        <h4 className="headline-section demi-font mt-3 mb-3 font-bold uppercase">
-          ORIGINAIS
-        </h4>
-      </div>
-      <div>
-        <h4 className="headline-section demi-font mt-3 mb-3 font-bold uppercase">
-          PREMIUM
-        </h4>
-      </div>
-      <div>
-        <h4 className="headline-section demi-font mt-3 mb-3 font-bold uppercase">
-          MÊS DO ORGULHO
-        </h4>
-      </div>
-      <div>
-        <h4 className="headline-section demi-font mt-3 mb-3 font-bold uppercase">
-          INKO
-        </h4>
-      </div>
-      <div>Banner</div>
-      <div>
-        <h4 className="headline-section demi-font mt-3 mb-3 font-bold uppercase">
-          SELEÇÕES
-        </h4>
-      </div>
-      <div>
-        <h4 className="headline-section demi-font mt-3 mb-3 font-bold uppercase">
-          INDEPENDENTES
-        </h4>
+      <div
+        id="contentContainer"
+        className=" mx-auto space-y-4 md:space-y-8 lg:space-y-12 xl:space-y-16 w-full md:w-2/3 lg:w-1/2 xl:w-1/3"
+      >
+        <div
+          id="bombandoContainer"
+          className="fold:h-20 sm:h-24 md:h-32 lg:h-36"
+        >
+          <h4 className="headline-section demi-font mt-3 mb-3 font-bold uppercase">
+            Bombando
+          </h4>
+          <Swiper
+            navigation
+            pagination={{ clickable: true, dynamicBullets: true }}
+            slidesPerView={1}
+            rewind={true}
+            autoplay={{
+              delay: 4000,
+              disableOnInteraction: false,
+              pauseOnMouseEnter: true,
+            }}
+            breakpoints={{
+              768: {
+                slidesPerView: 2,
+              },
+              1250: {
+                slidesPerView: 3,
+              },
+            }}
+            className="w-full  h-full"
+          >
+            {mockToRemove.map((item, index) => (
+              <SwiperSlide key={index} className="w-full h-full">
+                <Image
+                  src={item.src}
+                  alt={item.alt}
+                  fill
+                  quality={100}
+                  style={{ objectFit: "scale-down" }}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div
+          id="originaisContainer"
+          className="fold:h-20 sm:h-24 md:h-32 lg:h-36"
+        >
+          <h4 className="headline-section demi-font mt-3 mb-3 font-bold uppercase">
+            ORIGINAIS
+          </h4>
+          <Swiper
+            navigation
+            pagination={{ clickable: true, dynamicBullets: true }}
+            slidesPerView={1}
+            rewind={true}
+            autoplay={{
+              delay: 4000,
+              disableOnInteraction: false,
+              pauseOnMouseEnter: true,
+            }}
+            breakpoints={{
+              768: {
+                slidesPerView: 2,
+              },
+              1250: {
+                slidesPerView: 3,
+              },
+            }}
+            className="w-full  h-full"
+          >
+            {mockToRemove.map((item, index) => (
+              <SwiperSlide key={index} className="w-full h-full">
+                <Image
+                  src={item.src}
+                  alt={item.alt}
+                  fill
+                  quality={100}
+                  style={{ objectFit: "scale-down" }}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div
+          id="premiumContainer"
+          className="fold:h-20 sm:h-24 md:h-32 lg:h-36"
+        >
+          <h4 className="headline-section demi-font mt-3 mb-3 font-bold uppercase">
+            PREMIUM
+          </h4>
+          <Swiper
+            navigation
+            pagination={{ clickable: true, dynamicBullets: true }}
+            slidesPerView={1}
+            rewind={true}
+            autoplay={{
+              delay: 4000,
+              disableOnInteraction: false,
+              pauseOnMouseEnter: true,
+            }}
+            breakpoints={{
+              768: {
+                slidesPerView: 2,
+              },
+              1250: {
+                slidesPerView: 3,
+              },
+            }}
+            className="w-full  h-full"
+          >
+            {mockToRemove.map((item, index) => (
+              <SwiperSlide key={index} className="w-full h-full">
+                <Image
+                  src={item.src}
+                  alt={item.alt}
+                  fill
+                  quality={100}
+                  style={{ objectFit: "scale-down" }}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div id="mesContainer" className="fold:h-20 sm:h-24 md:h-32 lg:h-36">
+          <h4 className="headline-section demi-font mt-3 mb-3 font-bold uppercase">
+            MÊS DO ORGULHO
+          </h4>
+          <Swiper
+            navigation
+            pagination={{ clickable: true, dynamicBullets: true }}
+            slidesPerView={1}
+            rewind={true}
+            autoplay={{
+              delay: 4000,
+              disableOnInteraction: false,
+              pauseOnMouseEnter: true,
+            }}
+            breakpoints={{
+              768: {
+                slidesPerView: 2,
+              },
+              1250: {
+                slidesPerView: 3,
+              },
+            }}
+            className="w-full  h-full"
+          >
+            {mockToRemove.map((item, index) => (
+              <SwiperSlide key={index} className="w-full h-full">
+                <Image
+                  src={item.src}
+                  alt={item.alt}
+                  fill
+                  quality={100}
+                  style={{ objectFit: "scale-down" }}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div id="inkoContainer" className="fold:h-20 sm:h-24 md:h-32 lg:h-36">
+          <h4 className="headline-section demi-font mt-3 mb-3 font-bold uppercase">
+            INKO
+          </h4>
+          <Swiper
+            navigation
+            pagination={{ clickable: true, dynamicBullets: true }}
+            slidesPerView={1}
+            rewind={true}
+            autoplay={{
+              delay: 4000,
+              disableOnInteraction: false,
+              pauseOnMouseEnter: true,
+            }}
+            breakpoints={{
+              768: {
+                slidesPerView: 2,
+              },
+              1250: {
+                slidesPerView: 3,
+              },
+            }}
+            className="w-full  h-full"
+          >
+            {mockToRemove.map((item, index) => (
+              <SwiperSlide key={index} className="w-full h-full">
+                <Image
+                  src={item.src}
+                  alt={item.alt}
+                  fill
+                  quality={100}
+                  style={{ objectFit: "scale-down" }}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div id="bannerSecundarioContainer" className="relative h-36">
+          <Image
+            src={"https://i.imgur.com/i3IT94k.png"}
+            alt="Banner-Secundario"
+            fill
+            quality={100}
+            style={{ objectFit: "scale-down" }}
+          />
+        </div>
+        <div
+          id="selecoesContainer"
+          className="fold:h-20 sm:h-24 md:h-32 lg:h-36"
+        >
+          <h4 className="headline-section demi-font mt-3 mb-3 font-bold uppercase">
+            SELEÇÕES
+          </h4>
+          <Swiper
+            navigation
+            pagination={{ clickable: true, dynamicBullets: true }}
+            slidesPerView={1}
+            rewind={true}
+            autoplay={{
+              delay: 4000,
+              disableOnInteraction: false,
+              pauseOnMouseEnter: true,
+            }}
+            breakpoints={{
+              768: {
+                slidesPerView: 2,
+              },
+              1250: {
+                slidesPerView: 3,
+              },
+            }}
+            className="w-full  h-full"
+          >
+            {mockToRemove.map((item, index) => (
+              <SwiperSlide key={index} className="w-full h-full">
+                <Image
+                  src={item.src}
+                  alt={item.alt}
+                  fill
+                  quality={100}
+                  style={{ objectFit: "scale-down" }}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div
+          id="independentesContainer"
+          className="fold:h-20 sm:h-24 md:h-32 lg:h-36"
+        >
+          <h4 className="headline-section demi-font mt-3 mb-3 font-bold uppercase">
+            INDEPENDENTES
+          </h4>
+          <Swiper
+            navigation
+            pagination={{ clickable: true, dynamicBullets: true }}
+            slidesPerView={1}
+            rewind={true}
+            autoplay={{
+              delay: 4000,
+              disableOnInteraction: false,
+              pauseOnMouseEnter: true,
+            }}
+            breakpoints={{
+              768: {
+                slidesPerView: 2,
+              },
+              1250: {
+                slidesPerView: 3,
+              },
+            }}
+            className="w-full  h-full"
+          >
+            {mockToRemove.map((item, index) => (
+              <SwiperSlide key={index} className="w-full h-full">
+                <Image
+                  src={item.src}
+                  alt={item.alt}
+                  fill
+                  quality={100}
+                  style={{ objectFit: "scale-down" }}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
       </div>
     </div>
   );
