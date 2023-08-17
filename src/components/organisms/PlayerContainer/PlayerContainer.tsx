24b0d35bb79db93bd6953799c7aede421ddf9724
Bione <joao.bione@hotmail.com>
2023-08-17 14:34:13 -0300

    improved style first episode. component and clean code

---
+++ src/components/organisms/PlayerContainer/PlayerContainer.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Image from "next/image";
+
+interface PlayerContainerProps {
+  data: {
+    name: string;
+  };
+  isEpisodeLoading: boolean;
+  episodeImage: {
+    images: Array<{
+      order: number;
+      url: string;
+    }>;
+  };
+}
+
+const PlayerContainer: React.FC<PlayerContainerProps> = ({
+  data,
+  isEpisodeLoading,
+  episodeImage,
+}) => {
+  console.log(episodeImage.images);
+  return (
+    <div
+      id="playerContainer"
+      className="flex flex-col items-center justify-center h-screen"
+    >
+      <h2 className="text-2xl mb-4">{data.name}</h2>
+      {!isEpisodeLoading &&
+        episodeImage.images.map((image, index) => (
+          <div className="w-full h-full relative" key={index}>
+            <Image
+              src={image.url}
+              alt={data.name}
+              fill
+              style={{ objectFit: "scale-down" }}
+              quality={100}
+            />
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default PlayerContainer;
--- src/pages/conteudo/[slug].tsx
+++ src/pages/conteudo/[slug].tsx
@@ -4,20 +4,20 @@ import { HeaderHome } from "@/components/organisms/HeaderHome";
 import FooterHomePage from "@/components/organisms/FooterHomePage/FooterHomePage";
 import { MyError } from "@/types/types";
 import { useQuery } from "react-query";
-import Image from "next/image";
 import {
   Comic,
   ComicWithImages,
   getAllByConteudoId,
   getAllImagesByEpisodioId,
 } from "@/api/episodio";
+import PlayerContainer from "@/components/organisms/PlayerContainer/PlayerContainer";
 
 SwiperCore.use([Navigation, Pagination, Autoplay]);
 
 export default function Home(): JSX.Element {
   const content = sessionStorage.getItem("content");
   const parsedContent = JSON.parse(content || "{}");
-  const { data, isLoading, error } = useQuery<Comic[], MyError>(
+  const { data, isLoading, error } = useQuery<Comic[], MyError>( // todos episodiso aqui
     "getAllByConteudoId",
     () => getAllByConteudoId(Number(parsedContent.id)),
     { enabled: !!parsedContent }
@@ -41,35 +41,20 @@ export default function Home(): JSX.Element {
       </div>
     );
   }
-  console.log(data);
   return (
-    <div className="grid grid-cols-1 fold:gap-0 sm:gap-2 md:gap-3 lg:gap-4 xl:gap-5">
+    <div className="grid grid-cols-1 fold:gap-0 sm:gap-2 md:gap-3 lg:gap-4 xl:gap-5 overflow-y-auto h-screen">
       <HeaderHome />
-      <div className="content-name">{parsedContent.nome}</div>
+      <div className="text-center">{parsedContent.nome}</div>
       <div
-        className="content-description"
+        className="mx-4 my-2"
         dangerouslySetInnerHTML={{ __html: parsedContent.descricao }}
       ></div>
-      {!isLoading && !error && data && data.length > 0 && (
-        <div id="playerContainer">
-          <h2>{data[0].nome}</h2>
-
-          {isEpisodeLoading ? (
-            <p>Loading episode images...</p>
-          ) : (
-            episodeImage?.images?.map((image, index) => (
-              <Image
-                key={index}
-                src={image.url}
-                alt={`Image ${image.order} for episode ${data[0].nome}`}
-                width={200}
-                height={200}
-                quality={100}
-                style={{ objectFit: "scale-down" }}
-              />
-            ))
-          )}
-        </div>
+      {!isLoading && !error && data && data.length > 0 && episodeImage && (
+        <PlayerContainer
+          data={{ name: data[0].nome }}
+          isEpisodeLoading={isEpisodeLoading}
+          episodeImage={episodeImage}
+        />
       )}
       <FooterHomePage />
     </div>
